8c8106c2230bffe1b792bf882e93764916919b7f
RMaxwell404 <maxwellryan404@gmail.com>
2021-05-07 02:23:04 -0400

    Functionality for PokemonService. getNewPokemon not yet implemented.

--- backend/src/main/java/com/revature/pokecare/service/PokemonService.java
+++ backend/src/main/java/com/revature/pokecare/service/PokemonService.java
@@ -1,6 +1,9 @@
 package com.revature.pokecare.service;
 
 import com.revature.pokecare.models.Pokemon;
+import com.revature.pokecare.repository.PokemonRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -8,20 +11,38 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class PokemonService {
+	
+	@Autowired
+	PokemonRepository pr;
 
     public Pokemon findMyPokemon(int id) {
+    	if (id > 0) {
+    		Pokemon po = pr.findPokemonById(id);
+    		if(po != null) {return po;}
+    	}
+    	
         return null;
     }
 
     public boolean deletePokemon(int id) {
-        return true;
+    	if(id != 0) {
+    		pr.deletePokemon(id);
+    		return true;
+    	}
+        return false;
     }
 
     public Pokemon getNewPokemon() {
+    	
+    	
         return null;
     }
 
     public boolean updatePokemon(Pokemon pk) {
-        return true;
+    	if (pk != null) {
+    		pr.updatePokemon(pk);
+    		return true;
+    	}
+        return false;
     }
 }
