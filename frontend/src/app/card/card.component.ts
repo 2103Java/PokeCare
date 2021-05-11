a6db927f04d1212ca4095c2b9af3680fe0804676
Coach-BWise <wisebry11@gmail.com>
2021-05-11 00:35:18 -0400

    all models now render

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -1,11 +1,12 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {animate, state, style, transition, trigger} from "@angular/animations";
 import {CardData} from "./card.component.interface";
+import {ifStmt} from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
     animations: [
         trigger('cardFlip', [
             state('default', style({
@@ -31,17 +32,38 @@ export class CardComponent implements OnInit {
     @Input() position: number;
 
     cardName: string;
-    pokeName: string = "pinsir";
+    //this is the name that defines what 3D model is returned.
+    pokeName: string = "sdf";
+    errorHandled: boolean = false;
 
+    constructor() {
+    }
 
-  constructor() { }
+    ngOnInit() {
+        this.cardName = 'Title ' + this.index;
+    }
 
-  ngOnInit(){
-      this.cardName = 'Title ' + this.index;
-  }
     get cardPosition() {
         return this.position;
     }
+
+    //if the 3D model for the pokemon does not exist; we change the source to a static img.
+    errHandler(error) {
+        console.log(error);
+
+        if (this.errorHandled) {
+            error.target.src = "https://www.pkparaiso.com/imagenes/xy/sprites/animados/" + this.pokeName + ".gif"
+        } else {
+            error.target.src = "https://www.pkparaiso.com/imagenes/ultra_sol_ultra_luna/sprites/animados-sinbordes-gigante/" + this.pokeName + ".gif"
+            this.errorHandled = true;
+        }
+        //error.target.src = "https://www.pkparaiso.com/imagenes/xy/sprites/animados/" + this.pokeName + ".gif"
+        //If the above fails I need to use the below url. but I don't know how to tell typescript the above also failed
+        // "https://www.pkparaiso.com/imagenes/ultra_sol_ultra_luna/sprites/animados-sinbordes-gigante/" + this.pokeName + ".gif"
+        // error.target.src = "https://www.pkparaiso.com/imagenes/xy/sprites/animados/" + this.pokeName + ".gif";
+        // error.target.style = "height:auto; width: 40%; top: 3rem"
+    }
+
 
     positionChange(position) {
         this.position = position;
