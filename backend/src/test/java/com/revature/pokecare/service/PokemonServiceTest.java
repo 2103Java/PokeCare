f32bc6bb9dfc76a2d01fc29a24cc1f8956dd90ef
RMaxwell404 <maxwellryan404@gmail.com>
2021-05-16 14:55:34 -0400

    Added a few test cases

---
+++ backend/src/test/java/com/revature/pokecare/service/PokemonServiceTest.java
@@ -0,0 +1,105 @@
+package com.revature.pokecare.service;
+
+import com.revature.pokecare.models.Pokemon;
+import com.revature.pokecare.models.Trainer;
+import com.revature.pokecare.repository.PokemonRepository;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokemonServiceTest extends TestCase {
+
+    @Mock
+    PokemonRepository mockRepo = Mockito.mock(PokemonRepository.class);
+    Trainer tr = Mockito.mock(Trainer.class);
+
+    PokemonService pkServe = new PokemonService(mockRepo);
+
+    @Before
+    public void setUpTests(){
+        Mockito.when(mockRepo.updatePokemon(Mockito.any())).thenReturn(true);
+    }
+
+    public void testGetNewPokemon() {
+        List<Pokemon> mockList = new ArrayList<Pokemon>();
+
+        Mockito.when(mockRepo.updatePokemon(Mockito.any())).thenReturn(true);
+        Mockito.when(tr.getPokemon()).thenReturn(mockList);
+        Pokemon pk = pkServe.getNewPokemon(tr);
+
+        assertTrue((pk.getHappiness() < 50 && pk.getHappiness() >= 0));
+        assertEquals(pk.getFatigue(), 0);
+        assertEquals(pk.getHunger(), 0);
+        assertTrue((pk.getNumber() > 0 && pk.getNumber() < 893));
+
+    }
+
+    public void testPlayWithPokemon() {
+        Pokemon tPoke = new Pokemon();
+
+        Pokemon pk = pkServe.playWithPokemon(tPoke);
+        assertTrue((pk.getHappiness() > 0));
+    }
+
+    public void testFeedPokemon() {
+        Pokemon tPoke = new Pokemon();
+        int testHung = 3;
+        tPoke.setHunger(testHung);
+        Pokemon pk = pkServe.feedPokemon(tPoke);
+
+        assertTrue(pk.getHunger() != testHung);
+        assertTrue(pk.getHunger() >= 0);
+        assertTrue(pk.getHunger() <= 100);
+    }
+
+    public void testTirePokemon() {
+        Pokemon tPoke = new Pokemon();
+        int testFat = 3;
+        tPoke.setFatigue(testFat);
+
+        Pokemon pk = pkServe.tirePokemon(tPoke);
+        assertTrue(tPoke.getFatigue() > testFat);
+        assertTrue(tPoke.getFatigue() >= 0);
+
+    }
+
+    public void testTrainPokemon1() {
+        Pokemon tPoke = new Pokemon();
+        tPoke.setFatigue(0);
+        tPoke.setHunger(0);
+
+        Pokemon pk = pkServe.trainPokemon(tPoke,1);
+        assertTrue(pk.getFatigue() == 4);
+        assertTrue(pk.getHunger() == 4);
+        assertTrue(pk.getExperience() == 5);
+    }
+
+    public void testTrainPokemon2() {
+        Pokemon tPoke = new Pokemon();
+        tPoke.setFatigue(0);
+        tPoke.setHunger(0);
+
+        Pokemon pk = pkServe.trainPokemon(tPoke,2);
+        assertTrue(pk.getFatigue() == 7);
+        assertTrue(pk.getHunger() == 2);
+        assertTrue(pk.getExperience() == 5);
+    }
+
+    public void testTrainPokemon3() {
+        Pokemon tPoke = new Pokemon();
+        tPoke.setFatigue(0);
+        tPoke.setHunger(0);
+
+        Pokemon pk = pkServe.trainPokemon(tPoke,3);
+        assertTrue(pk.getFatigue() == 2);
+        assertTrue(pk.getHunger() == 7);
+        assertTrue(pk.getExperience() == 5);
+    }
+
+    public void testRestPokemon() {
+    }
+}
---
+++ backend/src/test/java/com/revature/pokecare/service/TrainerServiceTest.java
@@ -0,0 +1,16 @@
+package com.revature.pokecare.service;
+
+import junit.framework.TestCase;
+
+public class TrainerServiceTest extends TestCase {
+
+    public void testLogin() {
+
+    }
+
+    public void testRegister() {
+    }
+
+    public void testDeleteTrainer() {
+    }
+}
